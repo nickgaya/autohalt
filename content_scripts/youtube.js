c2bcb7d2b563b21c08d422d7f0632486e6cd6747
Nick Gaya <nicholasgaya+github@gmail.com>
2020-08-14 16:19:37 -0700

    Clean up YouTube content script

--- content_scripts/youtube.js
+++ content_scripts/youtube.js
@@ -1,46 +1,51 @@
-const autoplaySelector = '[aria-label=Autoplay][aria-pressed]';
+function findAutoplayButton() {
+    return document.querySelector(
+        'ytd-compact-autoplay-renderer paper-toggle-button');
+}
 
 function disableAutoplay() {
-    var autoplayButton = document.querySelector(autoplaySelector);
-    if (autoplayButton) {
-        var pressed = autoplayButton.getAttribute('aria-pressed');
-        console.log("Autoplay button pressed: ", pressed);
-        if (pressed === 'true') {
-            autoplayButton.click();
-            console.log("Autoplay button clicked, now pressed: ",
-                        autoplayButton.getAttribute('aria-pressed'));
-        }
-        return true;
-    } else {
-        // console.log("Autoplay button not found");
+    var autoplayButton = findAutoplayButton();
+    if (!autoplayButton) {
+        // console.debug("Autoplay button not found");
         return false;
+    }
+    // console.debug("Found autoplay button:", autoplayButton);
+
+    if (autoplayButton.getAttribute('aria-pressed') === 'true') {
+        console.info("Clicking autoplay button");
+        autoplayButton.click();
+    }
+    return true;
+}
+
+// Watch for DOM changes until the autoplay element appears, then disable it.
+// YouTube is relatively user-friendly in that it respects the user's autoplay
+// setting across the browser session, so we can stop listening for mutation
+// events once the button is detected and toggled.
+
+var scheduled = false;
+
+function idleCallback() {
+    if (disableAutoplay()) {
+        // console.debug("Disconnecting MutationObserver");
+        observer.disconnect();
+    }
+    scheduled = false;
+}
+
+function scheduleIdleCallback() {
+    if (!scheduled) {
+        window.requestIdleCallback(idleCallback, {timeout: 1000});
+        scheduled = true;
     }
 }
 
-if (!disableAutoplay()) {
-    var scheduled = false;
+const observer = new MutationObserver(scheduleIdleCallback);
 
-    const idleCallback = function() {
-        if (disableAutoplay()) {
-            observer.disconnect();
-        }
-        scheduled = false;
-    };
+const observerConfig = {
+    subtree: true,
+    childList: true,
+};
+observer.observe(document.body, observerConfig);
 
-    const observerCallback = function(mutationsList, observer) {
-        if (!scheduled) {
-            window.requestIdleCallback(idleCallback, {timeout: 1000});
-            scheduled = true;
-        }
-    };
-
-    const observer = new MutationObserver(observerCallback);
-
-    const observerConfig = {
-        subtree: true,
-        childList: true,
-        attributes: true,
-        attributeFilter: ['aria-label', 'aria-pressed'],
-    };
-    observer.observe(document.body, observerConfig);
-}
+scheduleIdleCallback();
